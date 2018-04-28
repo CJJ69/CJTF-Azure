586f3aeb5be619a6b82058bb365fe6e3a390d492
CJJ69 <chris.jones@xite.co.uk>
2018-04-28 16:43:21 +0100

    remove tags block

--- cjtest.tf
+++ cjtest.tf
@@ -109,10 +109,6 @@ resource "azurerm_lb_backend_address_pool" "backendaddresspool" {
   name                = "${var.name_prefix}-BackEndAddressPool"
   resource_group_name = "${azurerm_resource_group.rg.name}"
   loadbalancer_id     = "${azurerm_lb.loadbalancer.id}"
-
-  tags {
-    environment = "#{Octopus.Environment.Name}"
-  }
 }
 
 # Network Security Group
